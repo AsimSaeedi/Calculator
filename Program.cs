e161a8afa8cd82ca783ee29e5f797279cd26368f
AsimSaeedi <aohs1444@gmail.com>
2024-10-19 22:00:24 +0300

    asim

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string enterNumOne;
+int numOne;
+
+string enterNumTwo;
+int NumTwo;
+
+int total;
+
+Console.Write("enter Number One: ");
+enterNumOne = Console.ReadLine();
+numOne = int.Parse(enterNumOne);
+
+Console.Write("calculator + | - | * | / : ");
+string enterCalculator = Console.ReadLine();
+
+Console.Write("enter Number Two: ");
+enterNumTwo = Console.ReadLine();
+ NumTwo = int.Parse(enterNumTwo);
+
+
+if(enterCalculator == "+")
+{
+    total = numOne + NumTwo;
+    Console.WriteLine(numOne + " " + enterCalculator + " " + NumTwo + " = " + total);
+}
+else if (enterCalculator == "-")
+{
+    total = numOne - NumTwo;
+    Console.WriteLine(numOne + " " + enterCalculator + " " + NumTwo + " = " + total);
+}
+else if (enterCalculator == "")
+{
+    total = numOne * NumTwo;
+    Console.WriteLine(numOne + " " + enterCalculator + " " + NumTwo + " = " + total);
+}
+
+else if (enterCalculator == "/")
+{
+    total = numOne / NumTwo;
+    Console.WriteLine(numOne + " " + enterCalculator + " " + NumTwo + " = " + total);
+}
